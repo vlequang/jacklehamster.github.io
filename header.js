d45e68ace87e104d3e53ac44e7537a45cddfa479
Jack Le Hamster <jacklehamster@gmail.com>
2015-04-27 11:57:15 -0700

    Update header.js

--- header.js
+++ header.js
@@ -36,7 +36,8 @@ if(window==top) {
             td.width = "33%";
             td.align = "right";
             td.style.verticalAlign = "top";
-            td.innerHTML = '<a target="_blank" href="https://www.twitter.com/jacklehamster"><img title="@jacklehamster" style="margin:3px" width=18 height=18 src="https://g.twimg.com/Twitter_logo_blue.png"/></a>';
+            td.innerHTML = '<a target="_blank" href="https://www.twitter.com/jacklehamster"><img title="@jacklehamster" style="margin:3px" width=18 height=18 src="https://g.twimg.com/Twitter_logo_blue.png"/></a>'+
+                '<iframe src="http://www.reddit.com/static/button/button1.html?url='+top.location.href+'"><iframe>';
             document.body.insertBefore(table,document.body.firstChild);
             if(window.noheader)
                 table.style.display = "none";
